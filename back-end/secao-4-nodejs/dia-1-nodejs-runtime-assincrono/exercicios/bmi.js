2f035963859027cee576709e6933aa0fae5fa8a7
Bona <bonaccorsimarcos@gmail.com>
2023-06-01 18:09:07 -0300

    [deploy] Exercicio 5 :rocket:

--- back-end/secao-4-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/bmi.js
+++ back-end/secao-4-nodejs/dia-1-nodejs-runtime-assincrono/exercicios/bmi.js
@@ -12,11 +12,31 @@ function handleBMI(weight, height) {
 }
 
 function main() {
-    const userWeight = readline.questionFloat('What’s your weight? ')
-    const userHeight = readline.questionInt('What’s your height? ')
-    const bmi = handleBMI(userWeight, userHeight);
+    const userWeight = readline.questionFloat('What’s your weight? ');
+    const userHeight = readline.questionInt('What’s your height? ');
+    const bmi = handleBMI(userWeight, userHeight).toFixed(2);
+    let situation;
 
-    console.log(`BMI: ${bmi.toFixed(2)}`);
+    if(bmi >= 18.5) {
+        situation = 'Abaixo do peso (magreza)';
+    };
+    if (bmi > 18.15 && bmi < 24.9) {
+        situation = 'Peso normal';
+    };
+    if (bmi > 25 && bmi < 29.9) {
+        situation = 'Acima do peso (sobrepeso)';
+    };
+    if (bmi > 30 && bmi < 34.9) {
+        situation = 'Obesidade grau I';
+    };
+    if (bmi > 35 && bmi < 39.9) {
+        situation = 'Obesidade grau II';
+    };
+    if (bmi > 40) {
+        situation = 'Obesidade grau III';
+    };
+
+    console.log(`BMI: ${bmi}  Situação: ${situation}`);
 }
 
 main();
