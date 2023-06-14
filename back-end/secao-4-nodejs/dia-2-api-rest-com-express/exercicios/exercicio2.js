f429c58281f4484720b3c8923e83f2d3d5a0be33
Bona <bonaccorsimarcos@gmail.com>
2023-06-14 15:03:30 -0300

    [deploy] Exercicio 2 :rocket:

---
+++ back-end/secao-4-nodejs/dia-2-api-rest-com-express/exercicios/exercicio2.js
@@ -0,0 +1,3 @@
+// 401 Unauthorized
+// Embora o padrão HTTP especifique "unauthorized", semanticamente, essa resposta significa "unauthenticated". Ou seja, o cliente deve se autenticar para obter a resposta solicitada.
+
