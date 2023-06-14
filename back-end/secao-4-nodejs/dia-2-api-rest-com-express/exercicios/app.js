d3129aa22e6e184afaf98b3618613bfab7dd278a
Bona <bonaccorsimarcos@gmail.com>
2023-06-14 15:16:35 -0300

    [deploy] Exercicio 3 :rocket:

---
+++ back-end/secao-4-nodejs/dia-2-api-rest-com-express/exercicios/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.status(200).send(<h1>Hello World</h1>))
+
+app.listen(3001, () => console.log('Server is running on port 3001'));
