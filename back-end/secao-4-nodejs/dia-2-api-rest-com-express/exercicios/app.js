05e71293b05c18d953d83fb26850421a56682f64
Bona <bonaccorsimarcos@gmail.com>
2023-06-20 11:56:46 -0300

    [wip] 4.2 :construction:

--- back-end/secao-4-nodejs/dia-2-api-rest-com-express/exercicios/app.js
+++ back-end/secao-4-nodejs/dia-2-api-rest-com-express/exercicios/app.js
@@ -3,6 +3,7 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 
 const moviesPath = path.resolve(__dirname, './movies.json')
 
@@ -10,11 +11,37 @@ const readFile = async () => {
     try {
         const data = await fs.readFile(moviesPath);
         return JSON.parse(data);
-    } catch (error) {
-        console.error(error);
+    } catch (err) {
+        console.error(err);
     } 
 
 };
+
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await readFile();
+        res.status(200).json(movies);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+})
+
+app.post('/movies', async (req, res) => {
+    try {
+        const { movie, price } = req.body;
+        const movies = await readFile();
+        const newMovie = {
+            id: movies[movies.length - 1].id + 1,
+            movie,
+            price,
+        }
+        const allMovies = JSON.stringify([...movies, newMovie]);
+        await fs.writeFile(moviesPath, allMovies);
+        res.status(201).json(newMovie);
+    } catch (err) {
+        res.status(500).send({message: err.message})
+    }
+});
 
 app.get('/movies/:id', async (req, res) => {
     try{
@@ -22,7 +49,8 @@ app.get('/movies/:id', async (req, res) => {
         const movie = movies.find(({ id }) => id === Number(req.params.id));
         res.status(200).json(movie);
     } catch (error) {
-        console.error(error);
+        console.error(err);
+        res.status(500).send({ message: err.message });
     }
 });
 
